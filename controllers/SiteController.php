420d388569dbd1ba53c475d96b714e669536e4b2
Koba Paul <gurupavel1983@gmail.com>
2015-11-12 21:13:21 +0200

    test entry

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -12,6 +12,24 @@ use app\models\ContactForm;
 class SiteController extends Controller
 {
 
+ public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // данные в $model удачно проверены
+
+            // делаем что-то полезное с $model ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // либо страница отображается первый раз, либо есть ошибка в данных
+            return $this->render('entry', ['model' => $model]);
+        }
+    }
+
+
+
 public function actionSay($message = 'Привет')
     {
         return $this->render('say', ['message' => $message]);
